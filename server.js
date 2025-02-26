007a4e48a76df0f6e6feebacae0aa6bb23f239f9
Berend Janssen <145255252+BerendJanssen@users.noreply.github.com>
2025-02-26 13:34:04 +0100

    Paar dingetjes aangemaakt

---
+++ server.js
@@ -0,0 +1,71 @@
+require('dotenv').config() 
+
+const express = require('express')
+const app = express()
+
+app
+    .use ('/', express.static('static'))
+    .use (express.urlencoded({extended: true}))
+
+    .set ('view engine', 'ejs')
+    .set ('views', 'view')
+
+    .get ('/', home)
+
+    .listen(2828)
+
+
+//Use MongoDB
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`
+
+const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+})
+
+// Try to open a database connection
+client.connect()
+  .then(() => {
+    console.log('Database connection established')
+})
+  .catch((err) => {
+    console.log(`Database connection error - ${err}`)
+    console.log(`For uri - ${uri}`)
+})
+
+// A sample route, replace this with your own routes
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+// Middleware to handle not found errors - error 404
+app.use((req, res) => {
+    // log error to console
+    console.error('404 error at URL: ' + req.url)
+    // send back a HTTP response with status code 404
+    res.status(404).send('404 error at URL: ' + req.url)
+})
+
+// Middleware to handle server errors - error 500
+app.use((err, req, res) => {
+    // log error to console
+    console.error(err.stack)
+    // send back a HTTP response with status code 500
+    res.status(500).send('500: server error')
+})
+
+// Start the webserver and listen for HTTP requests at specified port
+app.listen(process.env.PORT, () => {
+    console.log(`I did not change this message and now my webserver is listening at port ${process.env.PORT}`)
+})
+
+
+
+function home(req, res) {
+    res.render('index.ejs')
+}
