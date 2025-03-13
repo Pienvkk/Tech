fe4502aa74210c6a2120511718868c610820e67c
JayFaasen <jaydeyfaassen@gmail.com>
2025-03-13 15:08:38 +0100

    carousel aan het proberen

--- static/script/home.js
+++ static/script/home.js
@@ -1,1 +1,22 @@
-console.log ('poep')
+// carousel
+var options = {
+    direction: 'horizontal',
+    loop: true,  // Booleaanse waarde in plaats van een string
+    speed: 300, 
+    cssMode: true, 
+  
+    // pagination
+    pagination: {
+      el: '.swiper-pagination', 
+      type: 'fraction' 
+    },
+  
+    // navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next', 
+      prevEl: '.swiper-button-prev' 
+    }
+};
+
+// Initialiseren van de Swiper
+const swiper = new Swiper('.swiper', options);
