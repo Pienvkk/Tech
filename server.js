86ad80d7f47f616a7d446228af986b64e5f1b459
Berend Janssen <145255252+BerendJanssen@users.noreply.github.com>
2025-03-17 13:08:28 +0100

    !!!

--- server.js
+++ server.js
@@ -82,21 +82,10 @@ client.connect()
         console.log('Database connection established')
         db = client.db(process.env.DB_NAME);
     })
+
     .catch((err) => {
-        console.error(`Database connection error - ${err}`)
-    })
-
-    .then(async() => {
-    console.log('Database connection established')
-
-    const db = client.db(process.env.DB_NAME)
-    const users = db.collection('0Users')
-
-})
-  .catch((err) => {
-    console.log(`Database connection error - ${err}`)
-    console.log(`For uri - ${uri}`)
-
+        console.log(`Database connection error - ${err}`)
+        console.log(`For uri - ${uri}`)
 })
 
 
@@ -111,7 +100,7 @@ app.post('/login', async (req, res) => {
 
     try {
         const users = db.collection('0Users')
-
+        const {season, team, driver} = req.body;
 
         // Zoek de gebruiker in de database
         const user = await users.findOne({ username: username, password: pass })
@@ -122,9 +111,6 @@ app.post('/login', async (req, res) => {
 
         // Login is succesvol - Sla de gebruiker op in de sessie
         req.session.user = { username: user.username }
-
-        // Login is succesvol - Redirect naar homepagina
-        res.redirect('/');
 
         // Update user om zijn preferences toe te voegen
         await users.updateOne(
