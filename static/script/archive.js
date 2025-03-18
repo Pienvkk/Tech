c5454cd92059e530c7d33ef113f8a32a3dcb2cfd
Pienvkk <Pienvk@hotmail.com>
2025-03-18 14:17:12 +0100

    leaderbord en playerbord opgemaakt

--- static/script/archive.js
+++ static/script/archive.js
@@ -1,6 +1,10 @@
+
+
+// DATA LADEN 
 document.addEventListener("DOMContentLoaded", function () {
     const radioButtonsArchive = document.querySelectorAll("input[name='categorie']");
     const dataList = document.querySelector(".archiveList");
+    
 
     function fetchData(category) {
         fetch(`/api/data/${category}`)
