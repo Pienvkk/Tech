9b7ed6de13983f394a9a6b33f11c0a113bd7c67e
Pienvkk <Pienvk@hotmail.com>
2025-03-20 14:08:04 +0100

    yes

--- static/script/home.js
+++ static/script/home.js
@@ -1,3 +1,23 @@
+//image laden 
+
+document.addEventListener("DOMContentLoaded", function() {
+  const images = document.querySelectorAll('.lazy-load');
+
+  const imageObserver = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+          if (entry.isIntersecting) {
+              const image = entry.target;
+              image.src = image.dataset.src;
+              observer.unobserve(image);
+          }
+      });
+  }, { rootMargin: "0px 0px 50px 0px" });
+
+  images.forEach(image => {
+      imageObserver.observe(image);
+  });
+});
+
 
 // carousel
 var options = {
