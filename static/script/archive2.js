5622af3230bd29b8911ad98dc8222865e8ebd8ab
Berend Janssen <145255252+BerendJanssen@users.noreply.github.com>
2025-03-25 14:25:34 +0100

    Inactieve JS bestanden weg + logo linkt weer goed naar homepagina + account preferences dubbele redirect verwijdert

--- static/script/archive2.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const radioButtonsArchive = document.querySelectorAll("input[name='categorie']");
-    const dataList = document.querySelector(".archiveList");
-})
