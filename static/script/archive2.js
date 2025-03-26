f1fee1a8f3cf138222693718c6b09cb83f499cb1
Pienvkk <Pienvk@hotmail.com>
2025-03-26 19:08:23 +0100

    Opmaak archief pagina + support pagina!

--- static/script/archive2.js
+++ static/script/archive2.js
@@ -1,29 +1,21 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const radioButtonsArchive = document.querySelectorAll("input[name='categorie']");
-    const dataList = document.querySelector(".archiveList");
-})
-
-
 
 //filteren en sorteren
-const AlphabetFilterButton = document.querySelector('.zoeken details button:first-of-type');
+const AlphabetFilterButton = document.querySelector('.zoeken details button:nth-of-type(1)')
 const ChronologischFilterButton = document.querySelector('.zoeken details button:last-of-type');
 const Zoekbalk = document.querySelector('.zoeken label input');
 
 
 
+
  // Initialize List.js
  const options = {
-    valueNames: ['forename', 'surname', 'name', 'season'] // Namen passen bij HTML
-};
-const list = new list('archiveList', options);
+    valueNames: ['forename', 'surname'] // Namen passen bij HTML
+}
+
+const ArchiefList = new List ('archiveList', options)
 
 //zoeken 
-
-if (AlphabetFilterButton) {
-    AlphabetFilterButton.addEventListener('click', function () {
-        list.sort('forename', { order: "asc" });
-    });
-} else {
-    console.error("AlphabetFilterButton niet gevonden!");
-}
+AlphabetFilterButton.addEventListener('click', function () {
+    ArchiefList.sort('forename', { order: "asc" })
+    console.log("Ik doe het")
+});
