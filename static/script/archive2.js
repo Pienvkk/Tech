1763efc3b60eceb846385b45e4b7bf9d51879c41
Pienvkk <Pienvk@hotmail.com>
2025-03-27 15:40:37 +0100

    ;((

--- static/script/archive2.js
+++ static/script/archive2.js
@@ -6,24 +6,12 @@ const Zoekbalk = document.querySelector('.zoeken label input');
 
 /*
 
- // Initialize List.js
- const options = {
-    valueNames: ['forename', 'surname'] // Namen passen bij HTML
-}
-
-const ArchiefList = new List ('archiveList', options)
-
-//zoeken 
-AlphabetFilterButton.addEventListener('click', function () {
-    ArchiefList.sort('forename', { order: "asc" })
-    console.log("Ik doe het")
-});
 */
  
 document.addEventListener('DOMContentLoaded', function () {
-    // Definieer de opties voor List.js
+
     const options = {
-        valueNames: ['forename', 'surname'] // Strings die overeenkomen met de HTML-klassen
+        valueNames: ['forename', 'surname'] 
     };
 
     const archiveList = new List('archiveList', options);
@@ -34,25 +22,6 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
-/* 
-
-options.sort(function (a, b) {
-    if (a.valueNames < b.valueNames) {
-      return -1;
-    }
-    if (a.valueNames > b.valueNames) {
-      return 1;
-    }
-    return 0;
-  });
-  console.log(valueNames);
-});
-
-
-AlphabetFilterButton.addEventListener('click', function () {
-let AlphabetSorteer = valueNames.sort();
-console.log(valueNames);
-});
-*/
+ 
 
 
