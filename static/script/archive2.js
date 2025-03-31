1359babbdbc523390914ad8034e9e5dce425117d
Pienvkk <Pienvk@hotmail.com>
2025-03-31 11:59:40 +0200

    beginsel en probeersel list

--- static/script/archive2.js
+++ static/script/archive2.js
@@ -1,11 +1,16 @@
-//filteren en sorteren
-const AlphabetFilterButton = document.querySelector('.sort')
+
+const List = require('list.js'); 
+
+const AlphabetFilterButton = document.querySelector('.sort');
 const ChronologischFilterButton = document.querySelector('.zoeken details button:last-of-type');
 const Zoekbalk = document.querySelector('.zoeken label input');
-  
-/*  probeersel  */
- 
+
+// check
 document.addEventListener('DOMContentLoaded', function () {
+    if (typeof List === 'undefined') {
+        console.error('List.js is niet geladen!!');
+        return;
+    }
 
     const options = {
         valueNames: ['forename', 'surname']  // Geef de classes door waarop we willen sorteren
@@ -14,12 +19,14 @@ document.addEventListener('DOMContentLoaded', function () {
     // Maak een nieuwe List.js instantie en wijs deze toe aan je lijst
     const archiveList = new List('archiveList', options);
 
-    
-    AlphabetFilterButton.addEventListener("click", function () {
-        archiveList.sort('surname', { order: "asc" });
+    AlphabetFilterButton.addEventListener('click', function () {
+        archiveList.sort('surname', { order: 'asc' });
+        console.log('Lijst gesorteerd op surname');
     });
-});              
 
- 
-
-
+    Zoekbalk.addEventListener('input', function () {
+        const searchValue = Zoekbalk.value; 
+        archiveList.search(searchValue); 
+        console.log(`Zoeken naar: ${searchValue}`);
+    });
+});
