51fd02ea5e0fe80064c4a63c94dc3dcc4d00e71c
JayFaasen <jaydeyfaassen@gmail.com>
2025-03-31 11:54:03 +0200

    list.js test

--- static/script/archive2.js
+++ static/script/archive2.js
@@ -1,27 +1,12 @@
-//filteren en sorteren
-const AlphabetFilterButton = document.querySelector('.sort')
-const ChronologischFilterButton = document.querySelector('.zoeken details button:last-of-type');
-const Zoekbalk = document.querySelector('.zoeken label input');
-
-
-/*
-
-*/
- 
-document.addEventListener('DOMContentLoaded', function () {
-
+document.addEventListener('DOMContentLoaded', function() {
+    // Initialiseer List.js
     const options = {
-        valueNames: ['forename', 'surname'] 
+        valueNames: ['forename', 'surname']  // Geef de classes door waarop we willen sorteren
     };
 
+    // Maak een nieuwe List.js instantie en wijs deze toe aan je lijst
     const archiveList = new List('archiveList', options);
-
-    
-    AlphabetFilterButton.addEventListener("click", function () {
-        archiveList.sort('surname', { order: "asc" });
-    });
 });
-
  
 
 
