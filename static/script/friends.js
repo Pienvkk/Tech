8f12947b655e223bec1463f882ffc22487639cc6
JayFaasen <jaydeyfaassen@gmail.com>
2025-04-04 20:33:43 +0200

    kleine dingetjes

---
+++ static/script/friends.js
@@ -0,0 +1,22 @@
+// const Zoekbalk = document.querySelector('.zoekenfriends input');
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     if (typeof List === 'undefined') {
+//         console.error('List.js is niet geladen!!');
+//         return;
+//     }
+
+//     const options = {
+//         valueName: ['friend', 'friend']  
+//     };
+
+//     const communityList = new List('friendsList', options);
+
+//     Zoekbalk.addEventListener('input', function () {
+//         const searchValue = Zoekbalk.value; 
+//         friendsList.search(searchValue); 
+//         console.log(`Zoeken naar: ${searchValue}`);
+//     });
+// });
+
+console.log("friends is geladen")
